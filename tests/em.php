2ce7d51166f6dfb6141bd74947c3e7e0c513d413
Ryan Chandler <ryangjchandler@gmail.com>
2021-10-12 23:39:33 +0100

    test: em

--- tests/em.php
+++ tests/em.php
@@ -1,9 +1,9 @@
 <?php
 
-use function Termwind\{em};
+use function Termwind\{em, span};
 
 it('can render em', function () {
     $em = em('string');
 
-    expect($em->toString())->toBe('<bg=default;options=>\e[3mstring\e[0m</>');
+    expect($em->toString())->toBe("<bg=default;options=>\e[3mstring\e[0m</>");
 });
