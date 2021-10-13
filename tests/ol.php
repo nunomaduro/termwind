ea933317b64043027f9d31edb6d21846e6d10558
Francisco Madeira <xico2k@gmail.com>
2021-10-13 13:52:24 +0100

    Fix Issue that throws when there is spaces or new lines between `<li>` elements

--- tests/ol.php
+++ tests/ol.php
@@ -12,3 +12,9 @@ it('renders only "li" as children', function () {
     expect(fn () => parse('<ol><div>list text 1</div></ol>'))
         ->toThrow(InvalidChild::class);
 });
+
+it('renders "li" elements and ignore empty spaces', function () {
+    $html = parse("<ol> <li>list text 1</li>\n\n\n</ol>");
+
+    expect($html)->toBe('<bg=default;options=><bg=default;options=>1. list text 1</></>');
+});
