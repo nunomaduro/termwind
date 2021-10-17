3bb775328e4caa72823e2285fdbf2df2c6408ab3
Ryan Chandler <ryangjchandler@gmail.com>
2021-10-17 22:13:29 +0100

    tests: dl

--- tests/dl.php
+++ tests/dl.php
@@ -1,14 +1,14 @@
 <?php
 
-use function Termwind\{dl, dt, dd};
-
 it('accepts multiple elements', function () {
-    $dl = dl([
-        dt('term'),
-        dd('details'),
-        dt('another term'),
-        dd('more details'),
-    ]);
+    $dl = parse(<<<'HTML'
+        <dl>
+            <dt>term</dt>
+            <dd>details</dd>
+            <dt>another term</dt>
+            <dd>more details</dd>
+        </dl>
+    HTML);
 
-    expect($dl->toString())->toBe("<bg=default;options=>\n<bg=default;options=bold>term</>\n    <bg=default;options=>details</>\n<bg=default;options=bold>another term</>\n    <bg=default;options=>more details</></>");
+    expect($dl)->toBe("<bg=default;options=>\n<bg=default;options=>\e[1mterm\e[0m</>\n    <bg=default;options=>details</>\n<bg=default;options=>\e[1manother term\e[0m</>\n    <bg=default;options=>more details</></>");
 });
