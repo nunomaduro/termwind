58f8304c223d77b23fea5861b2aabde8d82b0105
Francisco Madeira <xico2k@gmail.com>
2022-02-01 13:43:07 +0000

    style: fixes

--- tests/hideOutput.php
+++ tests/hideOutput.php
@@ -2,7 +2,6 @@
 
 use Symfony\Component\Console\Output\NullOutput;
 use function Termwind\hideOutput;
-use function Termwind\renderUsing;
 use Termwind\Termwind;
 
 it('can hide output', function () {
