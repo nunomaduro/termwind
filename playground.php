8bee60ee3ff116225e5643e39bd7783430dba8c3
Francisco Madeira <xico2k@gmail.com>
2022-02-18 12:14:18 +0000

    feat: Add `MediaQuery` Support.

--- playground.php
+++ playground.php
@@ -2,13 +2,17 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
-use function Termwind\render;
+use function Termwind\{render, terminal};
 
 render(<<<'HTML'
-    <div>
-        <div class="px-1 bg-green-600">Termwind</div>
-        <em class="ml-1">
-          Give your CLI apps a unique look
-        </em>
+    <div class="mx-2 my-1 space-y-1">
+        <div>
+            <span class="bg-green-500 text-black font-bold px-1">Media Queries with <b>Termwind</b>?</span>
+            <em class="ml-2">Sure, why not?</em>
+        </div>
+
+        <div class="px-1 font-bold bg-blue-500 sm:bg-red-600">
+            If bg is blue is small, if red > than small
+        </div>
     </div>
 HTML);
---
+++ tests/mediaQueries.php
@@ -0,0 +1,26 @@
+<?php
+
+use Termwind\Actions\StyleToMethod;
+
+it('supports styling', function ($name) {
+    putenv('COLUMNS=' . StyleToMethod::MEDIA_QUERIES_BREAKPOINTS[$name]);
+
+    $html = parse(<<<HTML
+        <div class="w-full {$name}:w-1"></div>
+    HTML);
+
+    expect($html)->toBe(' ');
+})->with(array_keys(StyleToMethod::MEDIA_QUERIES_BREAKPOINTS));
+
+it('renders based on the size even if the styles are in the wrong order', function () {
+    putenv('COLUMNS=64');
+
+    $html = parse(<<<HTML
+        <div class="md:bg-blue lg:bg-purple sm:bg-red bg-green">
+            Test
+        </div>
+    HTML);
+
+    expect($html)->toBe('<bg=red>Test</>');
+
+});
