85686b319dc0d5251cc0d07f88612b98af430088
Francisco Madeira <xico2k@gmail.com>
2022-05-11 19:06:33 +0100

    fix: Add RegExp instead of methods.

--- src/Actions/StyleToMethod.php
+++ src/Actions/StyleToMethod.php
@@ -45,7 +45,7 @@ final class StyleToMethod
      */
     public static function multiple(Styles $styles, string $stylesString): Styles
     {
-        $stylesString = array_merge($styles->defaultStyles(), self::parseStyles($stylesString));
+        $stylesString = array_merge($styles->defaultStyles(), preg_split('/(?![^\[]*\])\s/', $stylesString));
         $stylesString = self::sortStyles($stylesString);
 
         foreach ($stylesString as $style) {
@@ -72,7 +72,8 @@ final class StyleToMethod
             return $this->styles;
         }
 
-        $method = $this->parseMethod($method, $arguments);
+        $method = preg_split('/(?![^\[]*\])-/', $method);
+        $method = array_slice($method, 0, count($method) - count($arguments));
 
         $methodName = implode(' ', $method);
         $methodName = ucwords($methodName);
@@ -122,69 +123,6 @@ final class StyleToMethod
         });
 
         return $styles;
-    }
-
-    /**
-     * Parses the full list of styles into an array with all the methods.
-     *
-     * @return string[]
-     */
-    private static function parseStyles(string $str): array
-    {
-        $str = trim($str);
-        $length = mb_strlen($str, 'UTF-8');
-
-        $classes = [];
-        $start = 0;
-        $inBrackets = false;
-
-        for ($i = 0; $i < $length; $i++) {
-            if ($str[$i] === '[') {
-                $inBrackets = true;
-            } elseif ($str[$i] === ']') {
-                $inBrackets = false;
-            }
-
-            if ((preg_match('/\s/', $str[$i]) === 1 || $i === $length - 1)
-                && ! $inBrackets) {
-                $classes[] = trim(substr($str, $start, ($i + 1) - $start));
-                $start = $i;
-            }
-        }
-
-        return $classes;
-    }
-
-    /**
-     * Parses the full method name into an array of arguments.
-     *
-     * @param  array<int|string, string|int>  $arguments
-     * @return string[]
-     */
-    private function parseMethod(string $method, array $arguments): array
-    {
-        $length = mb_strlen($method, 'UTF-8');
-
-        $start = 0;
-        $inBrackets = false;
-        $items = [];
-
-        for ($i = 0; $i < $length; $i++) {
-            if ($method[$i] === '[') {
-                $inBrackets = true;
-            } elseif ($method[$i] === ']') {
-                $inBrackets = false;
-            }
-
-            $isLastChar = $i === $length - 1;
-
-            if (($method[$i] === '-' || $isLastChar) && ! $inBrackets) {
-                $items[] = trim(substr($method, $start, ($i + ($isLastChar ? 1 : 0)) - $start));
-                $start = $i + 1;
-            }
-        }
-
-        return array_slice($items, 0, count($items) - count($arguments));
     }
 
     /**
