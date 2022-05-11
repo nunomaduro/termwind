2e183731fc211be51d0954dfecc5a6da2f2a08fe
Francisco Madeira <xico2k@gmail.com>
2022-05-11 19:22:09 +0100

    tests: Fix phpstan issues.

--- src/Actions/StyleToMethod.php
+++ src/Actions/StyleToMethod.php
@@ -45,8 +45,10 @@ final class StyleToMethod
      */
     public static function multiple(Styles $styles, string $stylesString): Styles
     {
-        $stylesString = array_merge($styles->defaultStyles(), preg_split('/(?![^\[]*\])\s/', $stylesString));
-        $stylesString = self::sortStyles($stylesString);
+        $stylesString = self::sortStyles(array_merge(
+            $styles->defaultStyles(),
+            array_filter((array) preg_split('/(?![^\[]*\])\s/', $stylesString))
+        ));
 
         foreach ($stylesString as $style) {
             $styles = (new self($styles, $style))->__invoke();
@@ -72,7 +74,7 @@ final class StyleToMethod
             return $this->styles;
         }
 
-        $method = preg_split('/(?![^\[]*\])-/', $method);
+        $method = array_filter((array) preg_split('/(?![^\[]*\])-/', $method));
         $method = array_slice($method, 0, count($method) - count($arguments));
 
         $methodName = implode(' ', $method);
