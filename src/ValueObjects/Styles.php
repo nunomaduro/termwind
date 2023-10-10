642f96d31019f59573d5c0b92206042cac5a0514
Nuno Maduro <enunomaduro@gmail.com>
2023-10-10 09:56:15 +0100

    chore: fixes styles

--- src/ValueObjects/Styles.php
+++ src/ValueObjects/Styles.php
@@ -15,6 +15,7 @@ use Termwind\Enums\Color;
 use Termwind\Exceptions\ColorNotFound;
 use Termwind\Exceptions\InvalidStyle;
 use Termwind\Repositories\Styles as StyleRepository;
+
 use function Termwind\terminal;
 
 /**
