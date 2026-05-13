fe6dbb8b077ce4d2c798f931b454c7c90bc082ee
nuno maduro <enunomaduro@gmail.com>
2026-05-13 23:31:51 +0100

    chore: style

--- tests/node.php
+++ tests/node.php
@@ -3,19 +3,19 @@
 use Termwind\ValueObjects\Node;
 
 it('it should return empty string for dom comment', function () {
-    $node = new Node(new \DOMComment('You will not be able to see this text.'));
+    $node = new Node(new DOMComment('You will not be able to see this text.'));
 
     expect($node->getAttribute('foo'))->toBe('');
 });
 
 it('it should return empty string for dom text', function () {
-    $node = new Node(new \DOMText('bar'));
+    $node = new Node(new DOMText('bar'));
 
     expect($node->getAttribute('foo'))->toBe('');
 });
 
 it('it should return bar for dom element', function () {
-    $doc = new \DOMDocument('1.0');
+    $doc = new DOMDocument('1.0');
     $node = $doc->createElement('div');
     $el = $doc->appendChild($node);
     $el->setAttribute('foo', 'bar');
